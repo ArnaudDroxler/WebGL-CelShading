81f4da7bfbeaefc5b84fc9ccaf882edae9f6a7cf
arnaud droxler <arnaud.droxler@gmail.com>
2017-02-09 19:31:47 +0100

    add classe object and refactor

--- js/Object.js
+++ js/Object.js
@@ -1,52 +1,158 @@
 
+
+var offset = 0.2;
+var outLineColor = [0.1,0.1,0.1];
+
 class Object {
     constructor(fileName){
         this.fileName = fileName;
         this.vertexBuffer = null;
         this.indexBuffer = null;
-        this.colorBuffer = null;
-        loadModel(fileName);
+        this.normalBuffer = null;
+        this.numberIndex = 0;
+        this.prgOutLine = gl.createProgram();
+        this.prgCel = gl.createProgram();
+
+        console.log(this.prgOutLine);
+        this.readyToDraw = false;
+        this.initProgramme();
+        this.load();
+
     }
 
-    load(filename){
-        var request = new XMLHttpRequest();
-        console.info('Requesting ' + filename);
-        request.open("GET",filename);
+  /*  get readyToDraw () {
+        return this.readyToDraw;
+    }
+    set readyToDraw (readyToDraw){
+        this.readyToDraw = readyToDraw;
+    }*/
 
-        request.onreadystatechange = function() {
+    initProgramme(){
+        var Ovx = getShader(gl, "outline-shader-vs");
+        var Ofg = getShader(gl, "outline-shader-fs");
+
+        var Cvx = getShader(gl, "cel-shader-vs");
+        var Cfg = getShader(gl, "cel-shader-fs");
+
+        gl.attachShader(this.prgOutLine, Ovx);
+        gl.attachShader(this.prgOutLine, Ofg);
+        gl.linkProgram(this.prgOutLine);
+        gl.deleteShader(Ovx);
+        gl.deleteShader(Ofg);
+        console.log(this.prgOutLine);
+
+        gl.attachShader(this.prgCel, Cvx);
+        gl.attachShader(this.prgCel, Cfg);
+        gl.linkProgram(this.prgCel);
+        gl.deleteShader(Cvx);
+        gl.deleteShader(Cfg);
+
+        if (!gl.getProgramParameter(this.prgOutLine, gl.LINK_STATUS)) {
+            alert("Could not initialise shaders Outline");
+        }
+        if (!gl.getProgramParameter(this.prgCel, gl.LINK_STATUS)) {
+            alert("Could not initialise shaders Cel");
+        }
+
+        this.prgOutLine.vertexPositionAttribute = gl.getAttribLocation(this.prgOutLine, 'aVertexPosition');
+        gl.enableVertexAttribArray(this.prgOutLine.vertexPositionAttribute);
+        this.prgOutLine.vertexNormalAttribute = gl.getAttribLocation(this.prgOutLine, 'aVertexNormal');
+        gl.enableVertexAttribArray(this.prgOutLine.vertexNormalAttribute);
+        this.prgOutLine.pMatrixUniform = gl.getUniformLocation(this.prgOutLine, 'uPMatrix');
+        this.prgOutLine.mvMatrixUniform = gl.getUniformLocation(this.prgOutLine, 'uMVMatrix');
+        this.prgOutLine.offsetUniform = gl.getUniformLocation(this.prgOutLine, 'uOffset');
+        this.prgOutLine.outLineColor = gl.getUniformLocation(this.prgOutLine, 'uOutLineColor');
+
+        console.log(this.prgOutLine);
+        this.prgCel.vertexPositionAttribute = gl.getAttribLocation(this.prgCel, 'aVertexPosition');
+        gl.enableVertexAttribArray(this.prgCel.vertexPositionAttribute);
+        this.prgCel.vertexNormalAttribute = gl.getAttribLocation(this.prgCel, 'aVertexNormal');
+        gl.enableVertexAttribArray(this.prgCel.vertexNormalAttribute);
+        this.prgCel.pMatrixUniform = gl.getUniformLocation(this.prgCel, 'uPMatrix');
+        this.prgCel.mvMatrixUniform = gl.getUniformLocation(this.prgCel, 'uMVMatrix');
+
+    }
+
+    load(){
+        var me = this;
+        var request = new XMLHttpRequest();
+        console.info('Requesting ' +this.fileName);
+        request.open("GET",this.fileName);
+        request.onreadystatechange = function () {
             if (request.readyState == 4) {
+
                 if(request.status == 404) {
-                    console.info(filename + ' does not exist');
+                    console.info(this.fileName + ' does not exist');
                 }
                 else {
-                    handleJSONModel(JSON.parse(request.responseText));
+                    me.handleJSONModel(JSON.parse(request.responseText));
                 }
             }
-        }
+        };
         request.send();
     }
 
     handleJSONModel(obj) {
-        numberIndex = obj.indices.length;
+        this.numberIndex = obj.indices.length;
 
         this.vertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(obj.vertices), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
         this.normalBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(obj.normals), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
         this.indexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(obj.indices), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
+        this.readyToDraw = true;
     }
 
     draw(){
+        console.log("Object draw");
+
+        gl.enable(gl.CULL_FACE);
+        gl.cullFace(gl.FRONT);
+        gl.useProgram(this.prgOutLine);
+
+
+        gl.uniformMatrix4fv(this.prgOutLine.pMatrixUniform, false, pMatrix);
+        gl.uniformMatrix4fv(this.prgOutLine.mvMatrixUniform, false, mvMatrix);
+        gl.uniform1f(this.prgOutLine.offsetUniform,offset);
+        gl.uniform3fv(this.prgOutLine.outLineColor,outLineColor);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.vertexAttribPointer(this.prgOutLine.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+        gl.vertexAttribPointer(this.prgOutLine.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+        gl.drawElements(gl.TRIANGLES, this.numberIndex, gl.UNSIGNED_SHORT,0);
+
+
+        gl.disable(gl.CULL_FACE);
+
+        gl.useProgram(this.prgCel);
+
+        gl.uniformMatrix4fv(this.prgCel.pMatrixUniform, false, pMatrix);
+        gl.uniformMatrix4fv(this.prgCel.mvMatrixUniform, false, mvMatrix);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.vertexAttribPointer(this.prgCel.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+        gl.vertexAttribPointer(this.prgCel.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+        gl.drawElements(gl.TRIANGLES, this.numberIndex, gl.UNSIGNED_SHORT,0);
 
     }
 }
--- js/celShading.js
+++ js/celShading.js
@@ -1,12 +1,12 @@
 
 var canvas;
 var gl;
-var prgOutLine;
-var prgCel;
-var vertexBuffer;
+
+var object;
+/*var vertexBuffer;
 var indexBuffer;
 var normalBuffer;
-var numberIndex;
+var numberIndex;*/
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
 var fov = 60;
@@ -16,8 +16,6 @@ var rotX = 0;
 var dragging = false;
 var oldMousePos = {x: 0, y: 0};
 
-var offset = 0.2;
-var outLineColor = [0.1,0.1,0.1];
 
 window.onload = function() {
     //Recuperation du contexte WebGL
@@ -38,67 +36,17 @@ window.onload = function() {
     canvas.onmousedown = handleMouseDown;
     canvas.onmouseup = handleMouseUp;
 
-    initProgram();
-    initBuffers();
-    loadModel("model/teapot.json");
+    object = new Object("model/teapot.json");
+
+    tick();
 };
 
 function tick() {
     requestAnimFrame(tick);
     drawScene();
 }
-
+/*
 function initProgram() {
-    //Creation du programme
-    var Ovx = getShader(gl, "outline-shader-vs");
-    var Ofg = getShader(gl, "outline-shader-fs");
-
-    var Cvx = getShader(gl, "cel-shader-vs");
-    var Cfg = getShader(gl, "cel-shader-fs");
-
-
-    prgOutLine = gl.createProgram();
-    gl.attachShader(prgOutLine, Ovx);
-    gl.attachShader(prgOutLine, Ofg);
-    gl.linkProgram(prgOutLine);
-    gl.deleteShader(Ovx);
-    gl.deleteShader(Ofg);
-
-    prgCel = gl.createProgram();
-    gl.attachShader(prgCel, Cvx);
-    gl.attachShader(prgCel, Cfg);
-    gl.linkProgram(prgCel);
-    gl.deleteShader(Cvx);
-    gl.deleteShader(Cfg);
-
-    if (!gl.getProgramParameter(prgOutLine, gl.LINK_STATUS)) {
-        alert("Could not initialise shaders Outline");
-    }
-    if (!gl.getProgramParameter(prgCel, gl.LINK_STATUS)) {
-        alert("Could not initialise shaders Cel");
-    }
-
-
-    mat4.identity(pMatrix);
-    mat4.perspective(pMatrix, degToRad(fov),gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0);
-    //Declaration des variables a donner au shader
-
-
-    prgOutLine.vertexPositionAttribute = gl.getAttribLocation(prgOutLine, 'aVertexPosition');
-    gl.enableVertexAttribArray(prgOutLine.vertexPositionAttribute);
-    prgOutLine.vertexNormalAttribute = gl.getAttribLocation(prgOutLine, 'aVertexNormal');
-    gl.enableVertexAttribArray(prgOutLine.vertexNormalAttribute);
-    prgOutLine.pMatrixUniform = gl.getUniformLocation(prgOutLine, 'uPMatrix');
-    prgOutLine.mvMatrixUniform = gl.getUniformLocation(prgOutLine, 'uMVMatrix');
-    prgOutLine.offsetUniform = gl.getUniformLocation(prgOutLine, 'uOffset');
-    prgOutLine.outLineColor = gl.getUniformLocation(prgOutLine, 'uOutLineColor');
-
-    prgCel.vertexPositionAttribute = gl.getAttribLocation(prgCel, 'aVertexPosition');
-    gl.enableVertexAttribArray(prgCel.vertexPositionAttribute);
-    prgCel.vertexNormalAttribute = gl.getAttribLocation(prgCel, 'aVertexNormal');
-    gl.enableVertexAttribArray(prgCel.vertexNormalAttribute);
-    prgCel.pMatrixUniform = gl.getUniformLocation(prgCel, 'uPMatrix');
-    prgCel.mvMatrixUniform = gl.getUniformLocation(prgCel, 'uMVMatrix');
 
 }
 
@@ -122,7 +70,7 @@ function initBuffers() {
     ];
 
 }
-
+*/
 function getShader(gl, id) {
     var script = document.getElementById(id);
     if (!script) {
@@ -165,49 +113,20 @@ function drawScene(){
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 
+    mat4.identity(pMatrix);
+    mat4.perspective(pMatrix, degToRad(fov),gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0);
+
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix,mvMatrix,[0.0, 0.0, -40.0]);
     mat4.rotate(mvMatrix, mvMatrix,degToRad(rotX), [1, 0, 0]);
     mat4.rotate(mvMatrix, mvMatrix,degToRad(rotY), [0, 1, 0]);
     //mat4.rotate(mvMatrix,mvMatrix, degToRad(180), [0, 1, 0]);
 
-    gl.enable(gl.CULL_FACE);
-    gl.cullFace(gl.FRONT);
-    gl.useProgram(prgOutLine);
+    if (object.readyToDraw){
+        object.draw();
+    }
 
 
-    gl.uniformMatrix4fv(prgOutLine.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(prgOutLine.mvMatrixUniform, false, mvMatrix);
-    gl.uniform1f(prgOutLine.offsetUniform,offset);
-    gl.uniform3fv(prgOutLine.outLineColor,outLineColor);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.vertexAttribPointer(prgOutLine.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.vertexAttribPointer(prgOutLine.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    gl.drawElements(gl.TRIANGLES, numberIndex, gl.UNSIGNED_SHORT,0);
-
-
-    gl.disable(gl.CULL_FACE);
-
-    gl.useProgram(prgCel);
-
-    gl.uniformMatrix4fv(prgCel.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(prgCel.mvMatrixUniform, false, mvMatrix);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.vertexAttribPointer(prgCel.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.vertexAttribPointer(prgCel.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    gl.drawElements(gl.TRIANGLES, numberIndex, gl.UNSIGNED_SHORT,0);
 
 
 }
